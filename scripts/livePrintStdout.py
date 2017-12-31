ae10864d37e606b323b35fa6d54d36d792d78555
playfulcorgi <unrulybeardedweekend@gmail.com>
2017-12-31 16:03:11 +0100

    Made subprocess prints flush immediately, so if they're nested within each other, the output isn't stuck.

--- scripts/livePrintStdout.py
+++ scripts/livePrintStdout.py
@@ -1,8 +1,10 @@
+import sys
 from printSubprocessStdout import printSubprocessStdout
 
 def livePrintStdout(process, colors=True):
     for line in process.stdout:
         printSubprocessStdout(line, colors)
+        sys.stdout.flush()
     print('Quitting subprocess.')
     process.wait()
     code = process.returncode
